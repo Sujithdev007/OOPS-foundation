38d1bf2a911b19f3b0493cacdaee0a54bad9e264
Sujithdev007 <sujithsundarbe@gmail.com>
2024-01-13 23:17:44 +0530

    basics

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+
+
+public class Main {
+    public static void main(String aegs[]){
+
+        Vechile v = new Vechile();
+        v.country();
+
+    }
+}
---
+++ Vechile.java
@@ -0,0 +1,24 @@
+
+
+class Vechile {
+    private String color ="blue";
+    int maxSpeed;
+
+    public Vechile() {
+        System.out.println(this.color);
+        this.maxSpeed= 70;
+    }
+
+    public Vechile(int maxSpeed){
+        this.maxSpeed=maxSpeed;
+
+    }
+
+    public void country (){
+        System.out.println("India");
+    }
+
+
+
+}
+
